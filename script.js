41c506dd2f9554f58286d0c81d95a687320c898d
mbalihobaliho-create <mbalihobaliho@gmail.com>
2026-03-19 10:04:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* =========================
    NAMA PACAR (UBAH DI SINI)
 ========================= */
-const pacar = "PURI";
+const pacar = "PURI ";
 let i = 0;
 const nameEl = document.getElementById("name");
 
